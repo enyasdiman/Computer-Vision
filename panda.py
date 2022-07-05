44c07aaecd19e3676e8329667da2064c22065f73
EDA NUR YASDIMAN <enyasdiman@gmail.com>
2022-07-05 11:23:49 +0300

    Add files via upload

---
+++ panda.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 11:02:13 2022
+
+@author: enyasdiman
+"""
+
+import cv2
+
+image = cv2.imread("red_panda.png")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imwrite("gray_panda.png", gray_image)
+
+#cv2.imshow("Gray Panda", gray_image)
+
+#cv2.imshow("Red Panda", image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
